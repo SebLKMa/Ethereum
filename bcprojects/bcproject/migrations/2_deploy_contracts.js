e821e05a397a9049b1995355c27e3b3e9eb9705d
Sebastian Ma <sebmalikkeung@gmail.com>
2019-01-22 11:50:43 +0800

    renaming for BCProject

--- bcprojects/bcproject/migrations/2_deploy_contracts.js
+++ bcprojects/bcproject/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var TNOProject = artifacts.require("./TNOProject.sol");
+var Project = artifacts.require("./BCProject.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TNOProject, "P1", "Project 1"); // deploy with constructor args for contract
+  deployer.deploy(Project, "P1", "Project 1"); // deploy with constructor args for contract
 };
